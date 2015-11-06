cef8ff6394bab0cb403c9fe5d687eb3c64e3e1b6
Thiago de Bastos <thiago@thiagodebastos.com>
2015-11-07 01:11:20 +1100

    move enabler logic to dynamic_injection.js

---
+++ source/js/dynamic_injection.js
@@ -0,0 +1,23 @@
+var adWidth = 300;
+var adHeight = 250;
+var adwords = true;
+var adPlatform = '';
+document.write('<meta name="ad.size" content="width=' + adWidth + ',height=' + adHeight + '">')
+
+switch (adPlatform) {
+  case 'doubleclick':
+    document.write('<script src="https://s0.2mdn.net/ads/studio/Enabler.js"></script>');
+    break;
+
+  case 'adwords':
+    document.write('<script src="https://tpc.googlesyndication.com/pagead/gadgets/html5/api/exitapi.js"></script>');
+    break;
+
+  case 'sizmek':
+    document.write('<script src="http://ds.serving-sys.com/BurstingScript/adKit/adkit.js"></script>');
+    document.write('<script src="js/vendor/sizmek.js"></script>');
+    break;
+
+  default:
+    document.write('<!-- WARNING: An enabler is not being injected into this banner -->');
+}

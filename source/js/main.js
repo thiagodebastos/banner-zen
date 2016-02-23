76df0151b1159d540cc6aad28de105e73631ee5e
Thiago de Bastos <thiago@thiagodebastos.com>
2016-02-23 12:44:43 +1100

    add greensock animation template

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,29 @@
 // call animation function below
 const animation = () => {
-  console.log('[custom] animation start');
+
+  // ANIMATION SETTINGS
+  const timelineProgress = (position) => {
+    console.log(`[custom] animation ${position}`);
+  }
+  const tl = new TimelineMax({
+    repeat: 0,
+    yoyo: false,
+    repeatDelay: 3,
+    onStart: timelineProgress,
+    onStartParams: ["start"],
+    onComplete: timelineProgress,
+    onCompleteParams: ["end"]
+  });
+
+
+  // ANIMATION CODE
+  tl.addLabel("frame01")
+
+
+
+  // ANIMATION DURATION REPORTING
+  console.log(`[custom] loop duration: ${tl.duration()}s`);
+  console.log(`[custom] total duration: ${tl.totalDuration()}s`);
+  // tl.seek("frame01")
+  // .pause();
 }

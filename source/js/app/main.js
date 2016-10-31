77193d3a5a6be24b4ebf8e8b8380efc85dd0172d
Thiago de Bastos <thiago@thiagodebastos.com>
2016-11-01 10:27:53 +1100

    clean up demo animation

--- source/js/app/main.js
+++ source/js/app/main.js
@@ -1,19 +1,49 @@
-/**
- * HTML5 banner main initialisation threshold.
- */
-let bannerStage;
-let bannerCreative;
+// call animation function below
+const animation = (() => {
 
-function init() {
-	bannerStage = new createjs.Stage(document.getElementById("bannerCanvas"));
-	// bannerCreative = new Banner();
+    const timelineProgress = (position) => {
+        console.log(`[animation] animation ${position}`)
+    }
 
-	// bannerStage.addChild(bannerCreative);
+    const masterTL = new TimelineLite(
+        {
+            repeat: -1
+              ,yoyo: false
+              ,repeatDelay: 0
+              ,onStart: timelineProgress
+              ,onStartParams: ['start']
+              ,onComplete: timelineProgress
+              ,onCompleteParams: ['end']
+        }
+  )
 
-	createjs.Ticker.setFPS(25);
-	createjs.Ticker.addEventListener("tick",handleTick);
-}
+    const ad = '#ad'
 
-function handleTick() {
-	bannerStage.update();
-}
+    /*
+    * FRAME 01
+    */
+    const frame01 = () => {
+        console.log('[animation] Frame 01')
+        const tl = new TimelineMax()
+        tl.to(ad, 0.5, {backgroundColor: 'white'})
+        return tl
+    }
+
+    /*
+    * FRAME 01
+    */
+    const frame02 = () => {
+        console.log('[animation] Frame 02')
+        const tl = new TimelineMax()
+
+        return tl
+    }
+
+
+    masterTL.add(frame01())
+    // .add(frame02())
+
+    return {
+        tlMain : masterTL,
+    }
+})
